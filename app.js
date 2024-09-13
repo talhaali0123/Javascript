1b8d4ca8bda8b879bb27bb4e9475bc8b16981abd
Gitterhub <GFGexample@gmail.orgg>
2024-09-13 21:08:45 +0500

    Add all files

---
+++ app.js
@@ -0,0 +1,63 @@
+// var weight = +prompt("Enter your weight");
+// var time = +prompt("Enter time");
+// var gender = prompt("Enter your gender");
+
+// if (weight > 100 && time < 6 && gender) {
+// console.log("Come to our tryout!");
+// } else {
+//     console.log("Come to our cookout!");
+// } 
+
+
+
+// let ma = parseFloat(prompt("Enter marks for subject 1:"));
+// let marks2 = parseFloat(prompt("Enter marks for subject 2:"));
+// let marks3 = parseFloat(prompt("Enter marks for subject 3:"));
+// let totalMarks = parseFloat(prompt("Enter total marks:"));
+
+// let obtainedMarks = marks1 + marks2 + marks3;
+// let percentage = (obtainedMarks / totalMarks) * 100;
+
+// let grade, remarks;
+// if (percentage >= 80) {
+//   grade = "A-one";
+//   remarks = "Excellent";
+// } else if (percentage >= 70) {
+//   grade = "A";
+//   remarks = "Good";
+// } else if (percentage >= 60) {
+//   grade = "B";
+//   remarks = "You need to improve";
+// } else {
+//   grade = "Fail";
+//   remarks = "Sorry";
+// }
+
+// alert(`Total Marks: ${totalMarks}\nMarks Obtained: ${obtainedMarks}\nPercentage: ${percentage}%\nGrade: ${grade}\nRemarks: ${remarks}`);
+
+
+// var progLang = prompt("Enter your programming language");
+
+// if(progLang === "JS") {
+//     var city = prompt("Enter your city");
+
+//     if (city === "Karachi") {
+//         var experience = +prompt("Enter your experience");
+
+//         if(experience >= 2){
+//             console.log("You are Hired!!");
+//         }
+//         else {
+//             console.log("Please experience gain");
+//         }
+//     } else {
+//         console.log(" First You are shifted to Karachi");
+//     }  
+// } else {
+//     console.log("Learn JS")
+// }
+
+var num = [10,20,30,40,50,60,70];
+var mixNum = [10,"Talha",true,"Arsalan",undefined,30];
+console.log(mixNum[1]);
+
